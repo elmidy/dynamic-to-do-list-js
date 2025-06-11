7b20166c89d05dcb7fbd8e3ecde1bd203cd7a928
Muhammed Elmidy <162920636+elmidy@users.noreply.github.com>
2025-06-11 14:02:31 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 document.addEventListener("DOMContentLoaded", function () {
+  const addButton = document.getElementById("add-task-btn");
+  const taskInput = document.getElementById("task-input");
+  const taskList = document.getElementById("task-list");
+
   addButton.addEventListener("click", addTask);
 
   taskInput.addEventListener("keypress", function (e) {
