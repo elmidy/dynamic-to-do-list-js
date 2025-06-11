3661abbd41e2a9efc7ad54f22f08ff7e6c208a6d
Muhammed Elmidy <162920636+elmidy@users.noreply.github.com>
2025-06-11 13:59:09 +0300

    js

---
+++ script.js
@@ -0,0 +1,28 @@
+addButton.addEventListener("click", addTask);
+
+taskInput.addEventListener("keypress", function (e) {
+  if (e.key === "Enter") {
+    addTask();
+  }
+});
+
+function addTask() {
+  const taskText = taskInput.value.trim();
+  if (taskText === "") {
+    alert("Enter a task");
+  } else {
+    const li = document.createElement("li");
+    li.textContent = taskText;
+
+    const delBtn = document.createElement("button");
+    delBtn.textContent = "Remove";
+    delBtn.className = "remove-btn";
+    delBtn.onclick = function () {
+      taskList.removeChild(li);
+    };
+
+    li.appendChild(delBtn);
+    taskList.appendChild(li);
+    taskInput.value = "";
+  }
+}
