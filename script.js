28bb2aa11d1905fe369c83b69c240b7ac3d0806b
Muhammed Elmidy <162920636+elmidy@users.noreply.github.com>
2025-06-11 14:00:10 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,30 @@
-addButton.addEventListener("click", addTask);
+document.addEventListener("DOMContentLoaded", function () {
+  addButton.addEventListener("click", addTask);
 
-taskInput.addEventListener("keypress", function (e) {
-  if (e.key === "Enter") {
-    addTask();
+  taskInput.addEventListener("keypress", function (e) {
+    if (e.key === "Enter") {
+      addTask();
+    }
+  });
+
+  function addTask() {
+    const taskText = taskInput.value.trim();
+    if (taskText === "") {
+      alert("Enter a task");
+    } else {
+      const li = document.createElement("li");
+      li.textContent = taskText;
+
+      const delBtn = document.createElement("button");
+      delBtn.textContent = "Remove";
+      delBtn.className = "remove-btn";
+      delBtn.onclick = function () {
+        taskList.removeChild(li);
+      };
+
+      li.appendChild(delBtn);
+      taskList.appendChild(li);
+      taskInput.value = "";
+    }
   }
 });
-
-function addTask() {
-  const taskText = taskInput.value.trim();
-  if (taskText === "") {
-    alert("Enter a task");
-  } else {
-    const li = document.createElement("li");
-    li.textContent = taskText;
-
-    const delBtn = document.createElement("button");
-    delBtn.textContent = "Remove";
-    delBtn.className = "remove-btn";
-    delBtn.onclick = function () {
-      taskList.removeChild(li);
-    };
-
-    li.appendChild(delBtn);
-    taskList.appendChild(li);
-    taskInput.value = "";
-  }
-}
